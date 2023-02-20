2c901d0b4672f36ded035adc67de364767fb662c
Philip Weinke <philip.weinke@digitalnoise.de>
2023-02-20 09:55:24 +0100

    Add support for task lists (#5)

---
+++ tests/Renderer/Inline/TaskListItemMarkerRendererTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wnx\CommonmarkMarkdownRenderer\Tests\Renderer\Inline;
+
+use League\CommonMark\Extension\TaskList\TaskListItemMarker;
+use PHPUnit\Framework\TestCase;
+use Wnx\CommonmarkMarkdownRenderer\Renderer\Inline\TaskListItemMarkerRenderer;
+use Wnx\CommonmarkMarkdownRenderer\Tests\Support\FakeChildNodeRenderer;
+
+class TaskListItemMarkerRendererTest extends TestCase
+{
+    private TaskListItemMarkerRenderer $renderer;
+
+    protected function setUp(): void
+    {
+        $this->renderer = new TaskListItemMarkerRenderer();
+    }
+
+    /** @test */
+    public function it_renders_unchecked_task_list_item()
+    {
+        $node = new TaskListItemMarker(false);
+        $fakeRenderer = new FakeChildNodeRenderer();
+
+        $result = $this->renderer->render($node, $fakeRenderer);
+
+        $this->assertIsString($result);
+        $this->assertEquals("[ ]", $result);
+    }
+
+    /** @test */
+    public function it_renders_checked_task_list_item()
+    {
+        $node = new TaskListItemMarker(true);
+        $fakeRenderer = new FakeChildNodeRenderer();
+
+        $result = $this->renderer->render($node, $fakeRenderer);
+
+        $this->assertIsString($result);
+        $this->assertEquals("[x]", $result);
+    }
+}
