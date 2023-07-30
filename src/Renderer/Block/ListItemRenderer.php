e4ae1338b924ccbd3ea5f36ca403a408712e3bec
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-07-30 10:33:58 +0200

    Replace line breaks in ListItem content with custom string

--- src/Renderer/Block/ListItemRenderer.php
+++ src/Renderer/Block/ListItemRenderer.php
@@ -12,6 +12,8 @@ use League\CommonMark\Renderer\ChildNodeRendererInterface;
 
 final class ListItemRenderer implements \League\CommonMark\Renderer\NodeRendererInterface
 {
+    public const INLINE_LINE_BREAK = '_COMMONMARK_MARKDOWN_RENDERER_LINE_BREAK_';
+
     /**
      * @param ListItem $node
      *
@@ -25,11 +27,13 @@ final class ListItemRenderer implements \League\CommonMark\Renderer\NodeRenderer
 
         $contents = $childRenderer->renderNodes($node->children());
 
-        /**
+        // If the ListItem contains a line break, replace the line break with a custom string.
+        // The custom line break string is being replaced with a _native_ line break again, when
+        // being rendered in a ListBlock.
+        // This workaround is required to support multi-line list items.
         if (str_contains($contents, "\n")) {
-            $contents = str_replace("\n", " ", $contents);
+            $contents = str_replace("\n", self::INLINE_LINE_BREAK, $contents);
         }
-        */
 
         if (str_starts_with($contents, '<') && ! $this->startsTaskListItem($node)) {
             $contents = "\n" . $contents;
